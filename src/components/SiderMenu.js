a4dc90bfdccec812ac0e219aee880d236b181fab
Anton Isaev <otecvit@gmail.com>
2018-09-27 11:17:28 +0300

    second commit

---
+++ src/components/SiderMenu.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu, Icon } from 'antd';
+const { Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class SiderMenu extends Component {
+    state = {
+        collapsed: false,
+    }
+
+    onCollapse = (collapsed) => {
+        this.setState (
+            {collapsed}
+        );
+    }
+
+    render() {
+        return (
+            <Sider
+                collapsible
+                collapsed={this.state.collapsed}
+                onCollapse={this.onCollapse}
+            >
+            <div className="logo" />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1"><Icon type="home" /><span>Рабочий стол</span></Menu.Item>
+                <Menu.Item key="2"><Icon type="block" /><span>Заказы</span></Menu.Item>
+                <Menu.Item key="3"><Icon type="team" /><span>Клиенты</span></Menu.Item>
+                <Menu.Item key="4"><Icon type="environment" /><span>Рестораны</span></Menu.Item>
+                <SubMenu key="sub1" title={<span><Icon type="bars" /><span>Меню</span></span>}>
+                    <Menu.Item key="5">Меню</Menu.Item>
+                    <Menu.Item key="6"><Link to="categories">Категории</Link></Menu.Item>
+                    <Menu.Item key="7">Блюда</Menu.Item>
+                    <Menu.Item key="8">Наборы</Menu.Item>
+                    <Menu.Item key="9">Блюда</Menu.Item>
+                    <Menu.Item key="10">Сортировка</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="11"><Icon type="idcard" /><span>Сотрудники</span></Menu.Item>
+                <Menu.Item key="12"><Icon type="gift" /><span>Акции</span></Menu.Item>
+                <SubMenu key="sub2" title={<span><Icon type="setting"/><span>Настройки</span></span>}>
+                    <Menu.Item key="13">Общие</Menu.Item>
+                    <Menu.Item key="14">Типы заказов</Menu.Item>
+                    <Menu.Item key="15">Время</Menu.Item>
+                    <Menu.Item key="16">Оплата</Menu.Item>
+                    <Menu.Item key="17">Оформление</Menu.Item>
+                    <Menu.Item key="18">Правила</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="19"><Icon type="credit-card" /><span>Оплата</span></Menu.Item>
+                <Menu.Item key="20"><Icon type="logout" /><span>Выход</span></Menu.Item>
+            </Menu>
+            </Sider>
+        );        
+    }
+}
+
+export default SiderMenu;
+
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {createStore, applyMiddleware, compose  } from 'redux';
 import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 
-import { Layout, Menu, Icon} from 'antd';
+import { Layout } from 'antd';
 
 import createHistory from 'history/createBrowserHistory';
 import { Route } from 'react-router';
@@ -15,12 +15,11 @@ import 'antd/dist/antd.css';
 
 import allReducers from './reducers/index';
 import Dashboard from './components/Dashboard';
+import SiderMenu from './components/SiderMenu';
 
 
 const history = createHistory();
-const {  Header, Content, Footer, Sider } = Layout;
-const SubMenu = Menu.SubMenu;
-
+const { Header, Content, Footer } = Layout;
 
 const store = createStore(allReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
@@ -46,56 +45,11 @@ const ContentPage =
 ;
 
 class MainClass extends React.Component {
-    state = {
-        collapsed: false,
-    }
-
-
-    onCollapse = (collapsed) => {
-        this.setState (
-            {collapsed}
-        );
-    }
 
     render() {
         return (
           <Layout style={{ minHeight: '100vh' }}>
-            <Sider
-                collapsible
-                collapsed={this.state.collapsed}
-                onCollapse={this.onCollapse}
-            >
-              <div className="logo" />
-              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1">
-                  <Icon type="pie-chart" />
-                  <span>Option 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="desktop" />
-                  <span>Option 2</span>
-                </Menu.Item>
-                <SubMenu
-                  key="sub1"
-                  title={<span><Icon type="user" /><span>User</span></span>}
-                >
-                  <Menu.Item key="3">Tom</Menu.Item>
-                  <Menu.Item key="4">Bill</Menu.Item>
-                  <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={<span><Icon type="team" /><span>Team</span></span>}
-                >
-                  <Menu.Item key="6">Team 1</Menu.Item>
-                  <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9">
-                  <Icon type="file" />
-                  <span>File</span>
-                </Menu.Item>
-              </Menu>
-            </Sider>
+              <SiderMenu/>
             <Layout>
               <Header style={{ background: '#fff', padding: 0 }} />
               <Content style={{ margin: '16px 16px', background: '#fff' }}>
@@ -110,11 +64,7 @@ class MainClass extends React.Component {
           </Layout>
         );
       }
-
-
 }
-
-
 
 ReactDOM.render(
     <Provider store = {store}>
