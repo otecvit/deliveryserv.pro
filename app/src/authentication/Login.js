d9ea4743392888854c5d0034cd27ce4c4c02c290
Anton Isaev <otecvit@gmail.com>
2019-05-28 11:41:19 +0300

    edit lofin form

--- app/src/authentication/Login.js
+++ app/src/authentication/Login.js
@@ -11,68 +11,48 @@ const RadioGroup = Radio.Group;
 const RadioButton = Radio.Button;
 const FormItem = Form.Item;
 
-
 class Login extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
         checkCookies: false,
         iStaffType: 0,
-      };
-}
+    };
 
-  componentDidMount()  {
-    // получаем cookies
-    const currentUser = Cookies.get('cookiename');
-    if (typeof currentUser !== 'undefined') {
-        
-        const url = this.props.optionapp[0].serverUrl + "/SelectOwner.php"; // изменяем категорию
-        fetch(url, {
-          method: 'POST',
-          headers: 
-          {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(
-          {
-            chUIDStaff: currentUser,
-          })
-        }).then((response) => response.json()).then((responseJsonFromServer) => {
-           
-            if (responseJsonFromServer.owner.length) {
-                this.props.onCheckUser(responseJsonFromServer.owner[0]);  // вызываем action
-            }
-
-            this.setState ({
-                checkCookies: true,
+    componentDidMount()  {
+      // получаем cookies
+      const currentUser = Cookies.get('cookiename');
+      if (typeof currentUser !== 'undefined') {
+          const url = `${this.props.optionapp[0].serverUrl}/SelectOwner.php`; // проверяем пользователя
+          fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(
+            {
+              chUIDStaff: currentUser,
             })
-
-        }).catch((error) => {
-            console.error(error);
-        });
-    } else {
-      this.setState ({
-          checkCookies: true,
-      })
-    }
-    
-  }  
+          }).then((response) => response.json()).then((responseJsonFromServer) => {
+              if (responseJsonFromServer.owner.length) {
+                  this.props.onCheckUser(responseJsonFromServer.owner[0]);  // вызываем action
+              }
+              this.setState ({
+                  checkCookies: true,
+              })
+          }).catch((error) => {
+              console.error(error);
+          });
+      } else {
+        this.setState ({
+            checkCookies: true,
+        })
+      }
+    }  
 
     handleSubmit = (e) => {
       e.preventDefault();
       this.props.form.validateFields((err, values) => {
         if (!err) {
-           
-            const url = this.props.optionapp[0].serverUrl + "/CheckOwner.php"; // изменяем категорию
+            const url = `${this.props.optionapp[0].serverUrl}/CheckOwner.php`; // проверяем владельца
             fetch(url, {
               method: 'POST',
-              headers: 
-              {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json',
-              },
               body: JSON.stringify(
               {
                 iStaffType: this.state.iStaffType,
@@ -80,107 +60,102 @@ class Login extends Component {
                 chHashPassword: values.password,
               })
             }).then((response) => response.json()).then((responseJsonFromServer) => {
-               
                 if (responseJsonFromServer.owner[0].status === "1") {
                     this.props.onCheckUser(responseJsonFromServer.owner[0]);  // вызываем action
-                    Cookies.set('cookiename', responseJsonFromServer.owner[0].chUIDStaff, { expires: 365 , path: '/' });
+                    Cookies.set('cookiename', responseJsonFromServer.owner[0].chUIDStaff, { expires: 365 , path: '/' }); // устанавливаем куки
                 } else {
                   message.error("Проверьте имя пользователя или пароль");
                 }
-
-
             }).catch((error) => {
                 console.error(error);
             });
- 
+        }
+      });
     }
-    });
-  }
 
-  onStaffType = (e) => {
-    this.setState({
-      iStaffType: e.target.value === "0" ? 0 : 1
-    });
-  }
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-
-    const { checkCookies, iStaffType } = this.state;
-
-    // загружаем данные с сервера
-    if (!checkCookies) return <LoadingScreen />;
-
-    if (typeof this.props.owner.chUID !== 'undefined') {
-      return <Redirect to="/"/>
-    }  
-
-    return (
-        <Modal
-          title="Авторизация"
-          centered
-          visible={true}
-          maskStyle={{backgroundColor: '#f2f2f2'}}
-          footer=""
-          width='350px'
-          className="form-login"
-        >
-          <Form onSubmit={this.handleSubmit}>
-          <FormItem
-            className="content-center"
-            formLayout='vertical'
-          >
-            {getFieldDecorator('radio-group', {
-              initialValue: "0"
-            })(
-              <RadioGroup
-                buttonStyle="solid"
-                onChange={this.onStaffType} 
-              >
-                <RadioButton  value="0">Владелец</RadioButton >
-                <RadioButton  value="1">Сотрудник</RadioButton >
-              </RadioGroup>
-            )}
-          </FormItem>
-          <Divider dashed />
-          <FormItem
-            label={!iStaffType ? "E-mail" : "Логин"}  
-            className="content-form"
-          >
-            {getFieldDecorator('chEmailOwner', {
-              rules: [{ required: true, message: 'Введите e-mail' }],
-            })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="" />
-            )}
-          </FormItem>
-          <FormItem
-             label="Пароль"  
-             className="content-form"
-          >
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Введите пароль' }],
-            })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="" />
-            )}
-          </FormItem>
-          <Divider dashed />
-          <FormItem
-            className="content-form">
-            <Button type="primary" htmlType="submit" className="button-login">
-                Войти
-            </Button>
-          </FormItem>
-          <Divider dashed />
-          </Form>
-          <p className="text-login">Забыли пароль - <Link to="reset-password">Сброс пароля</Link></p>
-          <p className="text-login">Нет учетной записи - <Link to="register">Регистрация</Link></p>
-          <p className="text-login-copyright">Deliveryserv © 2019 - <Link to="">Условия использования</Link></p>
-        </Modal>
-        );
+    // устанавливаем статус пользователя (админ или сотрудник)
+    onStaffType = (e) => {
+      this.setState({
+        iStaffType: e.target.value === "0" ? 0 : 1
+      });
     }
+
+    render() {
+
+      const { getFieldDecorator } = this.props.form;
+      const { checkCookies, iStaffType } = this.state;
+
+      // загружаем данные с сервера
+      if (!checkCookies) return <LoadingScreen />;
+      if (typeof this.props.owner.chUID !== 'undefined') {
+        return <Redirect to="/"/>
+      }  
+
+      return (
+          <Modal
+            title="Авторизация"
+            centered
+            visible={true}
+            maskStyle={{backgroundColor: '#f2f2f2'}}
+            footer=""
+            width='350px'
+            className="form-login"
+          >
+            <Form onSubmit={this.handleSubmit}>
+            <FormItem
+              className="content-center"
+              formLayout='vertical'
+            >
+              {getFieldDecorator('radio-group', {
+                initialValue: "0"
+              })(
+                <RadioGroup
+                  buttonStyle="solid"
+                  onChange={this.onStaffType} 
+                >
+                  <RadioButton  value="0">Владелец</RadioButton >
+                  <RadioButton  value="1">Сотрудник</RadioButton >
+                </RadioGroup>
+              )}
+            </FormItem>
+            <Divider dashed />
+            <FormItem
+              label={!iStaffType ? "E-mail" : "Логин"}  
+              className="content-form"
+            >
+              {getFieldDecorator('chEmailOwner', {
+                rules: [{ required: true, message: 'Введите e-mail' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="" />
+              )}
+            </FormItem>
+            <FormItem
+              label="Пароль"  
+              className="content-form"
+            >
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Введите пароль' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="" />
+              )}
+            </FormItem>
+            <Divider dashed />
+            <FormItem
+              className="content-form">
+              <Button type="primary" htmlType="submit" className="button-login">
+                  Войти
+              </Button>
+            </FormItem>
+            <Divider dashed />
+            </Form>
+            <p className="text-login">Забыли пароль - <Link to="reset-password">Сброс пароля</Link></p>
+            <p className="text-login">Нет учетной записи - <Link to="register">Регистрация</Link></p>
+            <p className="text-login-copyright">Deliveryserv © 2019 - <Link to="">Условия использования</Link></p>
+          </Modal>
+          );
+      }
 }
 const LoginForm = Form.create()(Login);
-
 export default connect (
   state => ({
     orders: state.orders,
